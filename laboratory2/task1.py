04af2f04a0cf0bd908de4785edb048a1b5f1cca3
BorysBohdan <b.borysenko-2023@kpi.ua>
2019-11-11 22:24:44 +0200

    завдання 1

--- laboratory2/task1.py
+++ laboratory2/task1.py
@@ -1,25 +1,24 @@
-def ex1():
-    lower_bound_of_sum=1
-    while 1:
-        try:
-            upper_bound_of_sum=int(input("Введіть верхню межу суми : "))+1
-            break
-        except ValueError:
-            print("Ви помилилися і ввели не число :)")
-    while 1 :
-        try:
-            variable=int(input("Введіть число замість змінної x : "))
-            break
-        except ValueError:
-            print("Ви помилилися і ввели не число :)")
-    result=0
-    while variable==0 :
-        try:
-            variable = float(input("Введіть число відмінне від 0 : "))
-            break
-        except ValueError:
-                print("Ви помилилися і ввели не число :)")
-    for lower_bound_of_sum in range (lower_bound_of_sum, upper_bound_of_sum):
-        result+=lower_bound_of_sum/(variable**lower_bound_of_sum)
-        lower_bound_of_sum+=1
-    print("Сумою є число " + str(result))
+import re
+
+re_number = re.compile("^[-+]?\d+\.?\d*$")
+
+def validator(pattern,promt):
+    text=input(promt)
+    while not bool(pattern.match(text)):
+        text = input(promt)
+    return text
+
+def float_greater_zero_validator(promt):
+    number=float(validator(re_number, promt))
+    return number
+
+lower_bound_of_sum=1
+
+upper_bound_of_sum=int(float_greater_zero_validator("Введіть верхню межу суми : "))+1
+variable=float_greater_zero_validator("Введіть число замість змінної x : ")
+result=0
+while variable==0 :
+    variable = float_greater_zero_validator("Введіть число замість змінної x : ")
+for bound in range (lower_bound_of_sum, upper_bound_of_sum):
+    result+=bound/(variable**bound)
+print("Сумою є число " + str(result))
