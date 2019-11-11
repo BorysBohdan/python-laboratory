fafd0f320602a887a0563b1549876f745c0e435c
BorysBohdan <b.borysenko-2023@kpi.ua>
2019-11-11 22:25:06 +0200

    завдання 2

--- laboratory2/task2.py
+++ laboratory2/task2.py
@@ -1,21 +1,30 @@
-def ex2():
-    while 1:
-        try:
-            number = int(input("Введіть число з якого хочете видалити цифру : "))
-            break
-        except ValueError:
-            print("Ви помилилися і ввели не число :)")
-    while 1:
-        try:
-            delete_number = int(input("Введіть цифру яку ви хочете видалити з числа " + str(number) + " : "))
-            break
-        except ValueError:
-            print("Ви помилилися і ввели не цифру :)")
-    List = list(str(number))
-    result = []
-    if str(delete_number) not in List:
-        print("Цифри " + str(delete_number) + " немає у числі " + str(number))
-    for a in List:
-        if str(delete_number) != a:
-            result.append(a)
-    print("Отримане число : "+"".join(result))
+import re
+
+re_number = re.compile("^[-+]?\d+\.?\d*$")
+re_digit = re.compile("^\d$")
+
+def validator(pattern,promt):
+    text=input(promt)
+    while not bool(pattern.match(text)):
+        text = input(promt)
+    return text
+
+def number_validator(promt):
+    number=float(validator(re_number, promt))
+    return number
+
+def digit_validator(promt):
+    number=int(validator(re_digit, promt))
+    return number
+
+number = number_validator("Введіть число з якого хочете видалити цифру : ")
+delete_number = digit_validator("Введіть цифру яку ви хочете видалити з числа " + str(number) + " : ")
+
+List = list(str(number))
+result = []
+if str(delete_number) not in List:
+    print("Цифри " + str(delete_number) + " немає у числі " + str(number))
+for number in List:
+    if str(delete_number) != number:
+        result.append(number)
+print("Отримане число : "+ "".join(result))
