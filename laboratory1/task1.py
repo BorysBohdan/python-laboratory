03354f25f461a6b514c2adecda6bf92c5f1b7399
BorysBohdan <b.borysenko-2023@kpi.ua>
2019-11-11 22:22:21 +0200

    завдання 1

--- laboratory1/task1.py
+++ laboratory1/task1.py
@@ -1,6 +1,18 @@
-def exercise1():
-    print("Обчислення відсотка одного числа від іншого")
-    first_number = float(input("Введіть число для якого будемо обчислювати відсоток  : "))
-    second_number = float(input("Введіть число відсоток від якого будемо обчислювати відсоток. : "))
-    result = str(second_number/first_number*100)
-    print("Відсоток числа " + str(second_number) + " від числа " + str(first_number) + " становить : " + result + " %")
+import re
+
+re_number = re.compile("^[-+]?\d+\.?\d*$")
+
+def validator(pattern,promt):
+    text=input(promt)
+    while not bool(pattern.match(text)):
+        text = input(promt)
+    return text
+
+def float_greater_zero_validator(promt):
+    number=float(validator(re_number, promt))
+    while number<=0:
+        number=float(validator(re_number, promt))
+    return number
+num1=float_greater_zero_validator("Введіть число для якого будемо обчислювати відсоток: ")
+num2=float_greater_zero_validator("Введіть число відсоток від якого будемо обчислювати відсоток: ")
+print("Відсоток числа " + str(num2) + " від числа " + str(num1) + " становить : " + str(num2/num1*100) + " %")
