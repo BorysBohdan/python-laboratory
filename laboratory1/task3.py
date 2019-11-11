26a916472a5790d4ca5402c6740390021d752d24
BorysBohdan <b.borysenko-2023@kpi.ua>
2019-11-11 22:24:21 +0200

    завдання 3

--- laboratory1/task3.py
+++ laboratory1/task3.py
@@ -1,9 +1,20 @@
-def exercise3():
-    print("Обчислення функції в залежності від значення введеної змінної")
-    x = float(input("Введіть число замість x : "))
-    if x >= 3 :
-       result = str((-x)**2+3*x+9)
-       print("Результатом обчислення є число " + result)
-    else :
-       result = str(1/(x**3-6))
-       print("Результатом обчислення є число " + result)
+import re
+
+re_number = re.compile("^[-+]?\d+\.?\d*$")
+
+def validator(pattern,promt):
+    text=input(promt)
+    while not bool(pattern.match(text)):
+        text = input(promt)
+    return text
+
+def float_greater_zero_validator(promt):
+    number=float(validator(re_number, promt))
+    return number
+
+x = float_greater_zero_validator("Введіть число замість x : ")
+
+if x >= 3:
+    print("Результатом обчислення є число " + str((-x) ** 2 + 3 * x + 9))
+else :
+    print("Результатом обчислення є число " + str(1/(x**3-6)))
